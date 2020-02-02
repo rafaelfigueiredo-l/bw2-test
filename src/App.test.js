81ea6aa21d67c6469e8a004c7191920dfe9c4cea
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2020-02-02 20:48:12 -0300

    Add Enzyme + Enzyme Adapter + Enzyme To Json Dev Dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("adds correctly", () => {
+  expect(1 + 1).toEqual(2);
+}); 
